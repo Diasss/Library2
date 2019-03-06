512353bf69ba4c50867a1e4bcf704eff1a9e1aba
Диас Оспанов <@local.itstep>
2019-03-06 13:59:05 +0600

    Добавьте файлы проекта.

---
+++ Library/Model/AdminService.cs
@@ -0,0 +1,54 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class AdminService
+    {
+        public void CreateReader()
+        {
+            using (var db = new LiteDatabase(@"Readers"))
+            {
+                var col = db.GetCollection<Reader>("reader");
+                string login = Console.ReadLine();
+                string password = Console.ReadLine();
+                string name = Console.ReadLine();
+                string address = Console.ReadLine();
+                string email = Console.ReadLine();
+                string telephoneNumber = Console.ReadLine();
+
+                Reader reader = new Reader(login, password, name, address, email, telephoneNumber);
+
+                col.Insert(reader);
+            }
+        }
+        public void ChangePasswordToReader()
+        {
+            Console.WriteLine("Введите логин пользователя, чтобы поменять пароль: ");
+            string login = Console.ReadLine();
+            Console.WriteLine("Введите пароль пользователя, чтобы поменять пароль");
+            string password = Console.ReadLine();
+            using (var db = new LiteDatabase(@"Readers"))
+            {
+                var col = db.GetCollection<Reader>("reader");
+                var result = col.FindAll();
+                foreach (Reader r in result)
+                {
+                    if (r.password == password && r.login == login)
+                    {
+                        Console.WriteLine("Введите новый пароль пользователя");
+                        string newPassword = Console.ReadLine();
+                        r.password = newPassword;
+                        col.Update(r);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Library/Model/ReaderService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    class ReaderService
+    {
+        public void ChangePassword(Reader reader)
+        {
+            Console.WriteLine("Введите новый пароль: ");
+            string newPassword = Console.ReadLine();
+            reader.password = newPassword;
+        }
+        public void CreateNewBookReader(Reader reader)
+        {
+            BookService.CreateBook(reader);
+        }
+        public void BanThatReader(Reader reader)
+        {
+            Console.WriteLine("Хотите ли вы заблокировать этого пользователя?(y-да, заблокировать; n- нет, не блокировать): ");
+            string chose = Console.ReadLine();
+            if (chose == "y")
+            {
+                reader.bannedUser = true;
+            }
+            else if (chose == "n")
+                reader.bannedUser = false;
+        }
+
+    }
+}
